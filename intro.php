781ed8bf2fe4d26d2f696ee15e8f3dd243d8ec8c
Alex J Reyes <arceldajactta@gmail.com>
2024-12-16 10:00:11 -0500

    first commit

---
+++ intro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro</title>
+
+    <link rel="icon" type="image/x-icon" href="./includes/favicon.ico">
+    <link rel = "stylesheet" href = "css\styles.css">
+</head>
+
+    <body class="container intro">
+        <header class="heading-1">Tutelage</header>
+    </body>
+</html>
